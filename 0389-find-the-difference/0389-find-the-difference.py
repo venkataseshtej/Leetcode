7949c87ebfe761d91ee39910f1afbbdc8d6ea45f
venkataseshtej <venkataseshtejmatta@gmail.com>
2025-02-21 08:11:16 -0500

    Time: 0 ms (100%), Space: 18 MB (24.45%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        res=0
+        for i in s:
+            res = res^ ord(i)
+        for j in t:
+            res =res ^ ord(j)
+        return chr(res)
+
+
+        
