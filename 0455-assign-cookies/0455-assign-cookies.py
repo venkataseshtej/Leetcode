139b0179ce21db0096de1491f2de1cec9ac6c9b3
venkataseshtej <venkataseshtejmatta@gmail.com>
2025-04-30 18:26:21 -0400

    Time: 23 ms (49.92%), Space: 19.8 MB (89.34%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -4,14 +4,18 @@ class Solution:
         s.sort()
         i=0
         j=0
-        # c=0
-        while i < len(g) and j < len(s):
-            if s[j]>=g[i]:
-                # c+=1
-                # j+=1
-                i+=1
-            j+=1
-        return i 
+        c=0
+        for j in range(len(s)):
+            if i < len(g) and s[j] >= g[i]:
+                c+=1
+                i+=1         
+        # while i < len(g) and j < len(s):
+        #     if s[j]>=g[i]:
+        #         # c+=1
+        #         # j+=1
+        #         i+=1
+        #     j+=1
+        return c
 
 
 
